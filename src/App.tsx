6f423a76f476e289b8ff63f554bc163adab53379
Steve Robertson <SteveJRobertson@users.noreply.github.com>
2020-02-06 22:18:36 +0000

    Feature/number of sessions display (#6)
    * feat: number of sessions display added
    
    * test: added tests for number of sessions display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { SessionTypeDisplay, TimerDigits } from './components';
+import {
+  NumberOfSessionsDisplay,
+  SessionTypeDisplay,
+  TimerDigits,
+} from './components';
 
 const App: React.FC = () => {
   const sessionLengthMins = 25;
+  const workIntervalCount = 4;
 
   return (
     <div className="App">
       <TimerDigits secondsRemaining={0} sessionLengthMins={sessionLengthMins} />
       <SessionTypeDisplay sessionLengthMins={sessionLengthMins} />
+      <NumberOfSessionsDisplay workIntervalCount={workIntervalCount} />
     </div>
   );
 };
---
+++ src/components/NumberOfSessionsDisplay/NumberOfSessionsDisplay.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { useState, useEffect } from 'react';
+import { MAX_WORK_INTERVAL_COUNT } from '../../constants';
+
+type NumberOfSessionsDisplayProps = {
+  workIntervalCount: number;
+};
+
+export const NumberOfSessionsDisplay: React.FC<NumberOfSessionsDisplayProps> = ({
+  workIntervalCount,
+}: NumberOfSessionsDisplayProps) => {
+  const [displayNumberOfSessions, setDisplayNumberOfSessions] = useState('');
+
+  useEffect(() => {
+    if (workIntervalCount === 3) {
+      setDisplayNumberOfSessions('Big break up next');
+    } else if (workIntervalCount === 4) {
+      setDisplayNumberOfSessions(
+        `${MAX_WORK_INTERVAL_COUNT} sessions remaining until your big break`,
+      );
+    } else {
+      setDisplayNumberOfSessions(
+        `${MAX_WORK_INTERVAL_COUNT -
+          workIntervalCount} sessions remaining until your big break`,
+      );
+    }
+  }, [workIntervalCount]);
+
+  const numberOfSessionsDisplayStyles = css`
+    text-align: center;
+  `;
+
+  return <p css={numberOfSessionsDisplayStyles}>{displayNumberOfSessions}</p>;
+};
